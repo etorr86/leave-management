d88d3febd675c669a4ba0574f70819b23ec3de20
Eduardo Torres <eduardo.torres2@dart.biz>
2021-11-04 12:26:48 -0400

    Added leave type controller

---
+++ Controllers/LeaveTypesController.cs
@@ -0,0 +1,97 @@
+﻿using AutoMapper;
+using leave_management.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Controllers
+{
+    public class LeaveTypesController : Controller
+    {
+        private readonly ILeaveTypeRepository leaveTypeRepository;
+        private readonly IMapper mapper;
+
+        public LeaveTypesController(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            this.leaveTypeRepository = leaveTypeRepository;
+            this.mapper = mapper;
+        }
+        // GET: LeaveTypesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LeaveTypesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LeaveTypesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeaveTypesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveTypesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveTypesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveTypesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveTypesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
